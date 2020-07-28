984855230effa301257d48ba57c7e2aa52dc673b
Victor Shinya <contato@victorshinya.com.br>
2020-07-27 22:18:40 -0300

    Update file header

---
+++ Assistant/View/Configuration/ConfigurationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConfigurationTableViewCell.swift
+//  Assistant
+//
+//  Created by Victor Shinya on 27/07/20.
+//  Copyright © 2020 Victor Shinya. All rights reserved.
+//
+
+import UIKit
+
+class ConfigurationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Assistant/View/Configuration/ConfigurationViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigurationViewModel.swift
+//  Assistant
+//
+//  Created by Victor Shinya on 27/07/20.
+//  Copyright © 2020 Victor Shinya. All rights reserved.
+//
+
+import Foundation
